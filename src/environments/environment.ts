caa8f64860e8bf4d305feb497b2c39fc50d5fe98
Amit Kumar <120473786+iamnomandra@users.noreply.github.com>
2025-05-11 23:02:58 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 declare let process: any;
 export const environment = {
   ENV: process.env.NODE_ENV,
-  API_BASE_URL: 'https://freeapi.gerasim.in/api/BigBasket/',
-  API_USER_URL: 'https://freeapi.gerasim.in/api/User/',
+  API_BASE_URL: '',
+  API_USER_URL: '',
 };
 
